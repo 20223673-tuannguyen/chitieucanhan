05441f4e7350bef7c9ea96814ed7f0cb526fc911
tuannguyen <20223673@gmail.com>
2026-04-22 00:14:02 +0700

    Update specific file

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blue = Color(0xFF2196F3);
+  static const Color green = Color(0xFF4CAF50);
+  static const Color accentPink = Color(0xFF9C27B0);
+  static const Color orange = Color(0xFFFF9800);
+  static const Color red = Color(0xFFF44336);
+  static const Color cyan = Color(0xFF00BCD4);
+}
